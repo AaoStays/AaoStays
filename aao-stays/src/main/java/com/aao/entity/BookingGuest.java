5a0f3b4e2d499dd32e04c2fe1c2510c9033960d9
raahulgv123-ctrl <raahulgv123@gmail.com>
2026-01-03 17:16:48 +0530

    BookingGuest.java

--- aao-stays/src/main/java/com/aao/entity/BookingGuest.java
+++ aao-stays/src/main/java/com/aao/entity/BookingGuest.java
@@ -1,7 +1,5 @@
 package com.aao.entity;
 
-import com.aao.dto.BookingGuestDto;
-
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -17,36 +15,15 @@ public class BookingGuest {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bookingGuestId;
 
-    @ManyToOne
-    @JoinColumn(name = "booking_id", nullable = false)
-    private Booking booking;
-
+    private Long bookingId;
     private String guestFullName;
     private String guestEmail;
     private String guestPhone;
-    private Integer guestAge;
-
-    private String guestIdProofType; 
+    private String guestIdProofType;
     private String guestIdProofNumber;
     private String relationshipToBooker;
-	public BookingGuestDto addGuestToBooking1(Long bookingId, BookingGuestDto dto) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public Object getGuestsByBooking(Long bookingId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public void deleteGuest1(Long guestId) {
-		// TODO Auto-generated method stub
-		
-	}
-	public BookingGuestDto addGuestToBooking(Long bookingId, BookingGuestDto dto) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public void deleteGuest(Long guestId) {
-		// TODO Auto-generated method stub
-		
-	}
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booking_id", insertable = false, updatable = false)
+    private Booking booking;
 }
