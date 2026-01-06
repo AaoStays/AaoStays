b8d3f179f72c497cadf0a960896ace810dea6dbf
raahulgv123-ctrl <raahulgv123@gmail.com>
2026-01-06 10:40:27 +0530

    Update PropertyAmenityRepository.java

--- aao-stays/src/main/java/com/aao/repo/PropertyAmenityRepository.java
+++ aao-stays/src/main/java/com/aao/repo/PropertyAmenityRepository.java
@@ -1,15 +1,13 @@
-package com.aao.repo;
+public interface PropertyAmenityRepository
+        extends JpaRepository<PropertyAmenity, Long> {
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.aao.entity.PropertyAmenity;
+    List<PropertyAmenity> findByProperty_PropertyId(Long propertyId);
 
-import java.util.List;
+    boolean existsByProperty_PropertyIdAndAmenity_AmenityId(
+            Long propertyId, Long amenityId
+    );
 
-public interface PropertyAmenityRepository extends JpaRepository<PropertyAmenity, Long> {
-
-    boolean existsByPropertyIdAndAmenityId(Long propertyId, Long amenityId);
-
-    List<PropertyAmenity> findByPropertyId(Long propertyId);
-
-    List<PropertyAmenity> findByAmenityId(Long amenityId);
+    void deleteByProperty_PropertyIdAndAmenity_AmenityId(
+            Long propertyId, Long amenityId
+    );
 }
